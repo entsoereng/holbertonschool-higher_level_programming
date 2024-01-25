e495666b82af903e1ebe45e2daa10546a90914e9
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-01-25 15:08:05 +0200

    Update 0-positive_or_negative.py

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,5 +5,7 @@ if number > 0:
   print("{number} is positive".format(number))
 elif number == 0:
   print("{number} is zero".format(number))
-else: number < 0:
+elif number < 0:
   print("{number} is negative".format(number))
+else:
+  print(number, end=" ")
