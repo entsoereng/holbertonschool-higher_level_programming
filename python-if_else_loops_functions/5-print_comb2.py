d5d3512ea4b7711fef699ac6b2895e826e1efcd2
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-01-25 17:54:43 +0200

    Update 5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for s in range(100):
-    print("{:02d}".format(s), end=',')
+        print("{:02d}".format(s), end=', ')
 if s <= 0 <= 100:
-    print("{:02d}".format(s))
+        print("{:02d}".format(s))
