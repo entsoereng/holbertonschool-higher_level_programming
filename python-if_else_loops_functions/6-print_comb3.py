9209ca7f3796b810620bc6e70332d00b3a0b4459
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-01-26 11:22:28 +0200

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for s in range(10):
     for x in range(s + 1, 10):
-        print("{:d}{:d}".format(s, x0, end=", " if s != 8 or x != 9 else "\n")
+        print("{:d}{:d}".format(s, x), end=", " if s != 8 or x != 9 else "\n")
