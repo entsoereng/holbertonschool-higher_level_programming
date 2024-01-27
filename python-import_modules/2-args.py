9626a49d76872fc2ef926aacfcb2c5bb197680ba
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-01-27 14:21:58 +0200

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from sys import argv
+
+if len(argv) == 1:
+    print("0 arguments.")
+    print(".")
+else:
+    print("{} argument{}:".format(len(argv) - 1, 's' if len(argv) > 2 else ''))
+    for i, arg in enumerate(argv[1:], start=1):
+        print("{}: {}".format(i, arg))
