0606fd916bd9fa4d0f0849d346d7aaef321949cd
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-01-27 13:58:24 +0200

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-__import__("0-add")
-a = 1
-b = 2
-add_0 = __import__('add_0').add
-print("{:d} + {:d} = {:d}".format(a, b, add_0(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
