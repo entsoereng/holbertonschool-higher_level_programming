528519b036788616c7eacf1469f92e66ea5f73ae
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-01-30 16:41:25 +0200

    Update 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        # Find the key with the maximum value
+        max_key = max(a_dictionary, key=a_dictionary.get)
+        return max_key
+    else:
+        # If the dictionary is empty, return None
+        return None
