74403c831eb241f8bed5e53a5a081412800840b9
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-02-04 18:14:23 +0000

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+"""
+1-square.py: is a class Square that defines a square
+"""
+
+
+class Square:
+    """class Square that defines a square
+
+    Attributes:
+        attr1 (size): Size of the Square.
+
+    """
+
+    def __init__(self, size=0):
+        """Initializer _Square__size"""
+        self.__size = size
