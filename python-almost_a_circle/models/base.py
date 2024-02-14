9e8961815f814de35e7af04e7623f7bd501fd3ad
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-02-14 11:19:26 +0200

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Defines a base model class.
+"""
+Class Base:
+    """
+    Represents the base model.
+    """
+    __nb_objects = 0
+    def __init__(self, id=none):
+        if id is not none:
+            self.id = id
+
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
