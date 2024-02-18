ea661d1828af381e76f0c129ca8ff09ff474a3c5
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-02-18 21:27:43 +0000

    Update test_base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -13,3 +13,10 @@ from models.square import Square
 
 class TestBase_create(unittest.TestCase):
     """Unittests for testing create method of Base class."""
+
+
+    def test_create_rectangle_original(self):
+        r1 = Rectangle(3, 5, 1, 2, 7)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(r1))
