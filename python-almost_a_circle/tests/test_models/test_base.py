80afde471db8f3fa42f233fcafabc2985fb37b88
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-02-18 21:29:50 +0000

    Update test_base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,22 +1,9 @@
-#!/usr/bin/python3
+class Base:
+    __nb_objects = 0
 
-
-"""Defines unittests for base.py."""
-
-
-import os
-import unittest
-from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
-
-
-class TestBase_create(unittest.TestCase):
-    """Unittests for testing create method of Base class."""
-
-
-    def test_create_rectangle_original(self):
-        r1 = Rectangle(3, 5, 1, 2, 7)
-        r1_dictionary = r1.to_dictionary()
-        r2 = Rectangle.create(**r1_dictionary)
-        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(r1))
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
