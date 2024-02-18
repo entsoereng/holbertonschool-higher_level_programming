2bcbe9cfc281c6981ebed9c4bfdcdcf7cac310da
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-02-18 21:22:01 +0000

    Update test_base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+
+"""Defines unittests for base.py."""
+
+
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestBase_create(unittest.TestCase):
+    """Unittests for testing create method of Base class."""
