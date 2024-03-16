c0f9d63dfb2bd9944fa56dc9382387f7a213979a
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-03-16 12:56:09 +0200

    Update 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS `cities`(
   `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   `state_id` INT NOT NULL,
   `name` VARCHAR(256) NOT NULL,
-  FOREIGN KEY (state_id) REFERENCES states(id));
+  FOREIGN KEY (`state_id`) REFERENCES `states`(`id`));
