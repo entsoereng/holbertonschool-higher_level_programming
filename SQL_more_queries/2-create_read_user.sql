8a6cb4b2271ba1908c040a1afc0c3c32595dca5b
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-03-16 11:57:22 +0200

    Create 2-create_read_user.sql

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Create the database hbtn_0d_2 and user_0d_2 with specific privileges.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+DROP USER IF NOT EXISTS 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2'@'localhost';
+FLUSH PRIVILEGES;
