a5858b2b1024d7bbccf577385dd3ff6e59639243
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-03-16 12:02:41 +0200

    Update 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Create the database hbtn_0d_2 and user_0d_2 with specific privileges.
+-- Create the database `hbtn_0d_2` and `user_0d_2` with specific privileges.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 DROP USER IF NOT EXISTS 'user_0d_2'@'localhost';
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
