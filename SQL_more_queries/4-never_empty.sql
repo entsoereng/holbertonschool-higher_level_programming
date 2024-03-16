3eea09dfd6d1be16d961096bb3d1e1d3d4d240ed
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-03-16 12:22:28 +0200

    Update 4-never_empty.sql

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- Create the table id_not_null i MySQL server with id default value.
 CREATE TABLE IF NOT EXISTS `id_not_null`(
-  `id`, INT, DEFAULT 1,
+  `id` INT DEFAULT 1, `name` VARCHAR(256) NOT NULL);
