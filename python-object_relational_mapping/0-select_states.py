7e9b35bfea8f8272990cdda58610616d8b6018e8
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-03-21 11:47:09 +0200

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Lists all states from the database hbtn_0e_0_usa """
-import MySQLdb.connector
+import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connector.connect(host=sys.argv[1], user=sys.argv[2], 
-                                   passwd=sys.argv[3], database=sys.argv[4], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
+                         passwd=sys.argv[2], database=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
