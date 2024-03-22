cbbec7403eacf62af5dc2c3b835582165d5e982c
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-03-22 16:20:07 +0200

    Update 4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -4,7 +4,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], database=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT cities.id, cities.name, states.name FROM
