0dd7902936ed67f6f92dbb6af248205b49a675da
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-03-22 16:04:25 +0200

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,13 +4,37 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
-                         passwd=sys.argv[2], database=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    # Check if all required arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        # Connect to MySQL database
+        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+        cur = db.cursor()
+
+        # Execute SQL query
+        cur.execute("""SELECT * FROM states WHERE name
+                    LIKE BINARY 'N%' ORDER BY states.id""")
+        
+        # Fetch and print results
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+    
+    finally:
+        # Close cursor and database connection
+        if 'cur' in locals() and cur is not None:
+            cur.close()
+        if 'db' in locals() and db is not None:
+            db.close()
