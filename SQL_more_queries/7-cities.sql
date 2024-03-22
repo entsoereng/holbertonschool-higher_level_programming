7d840475d4b9f0218eccd5e5e8f46ecbcacc904e
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-03-22 16:54:36 +0200

    Update 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,11 +1,8 @@
 -- SQL script to create the database hbtn_0d_usa and the table cities on the MySQL server
-
 -- Query to create the database if it does not already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
 -- Query to use the database hbtn_0d_usa
 USE hbtn_0d_usa;
-
 -- Query to create the table if it does not already exist
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
