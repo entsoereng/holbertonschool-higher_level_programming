7739b0b365088119fdd4347c0926f14d27e1b024
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-03-22 16:06:57 +0200

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,40 +1,45 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa """
+"""
+Script to list all states with a name starting with
+'N' (upper N) from the database hbtn_0e_0_usa.
+"""
+
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # Check if all required arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
 
-    # Extract command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+def filter_states(username, password, database):
+    """
+    Connects to the MySQL server and lists all states
+    with a name starting with 'N'.
 
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Database name.
+    """
     try:
-        # Connect to MySQL database
-        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
-        cur = db.cursor()
-
-        # Execute SQL query
-        cur.execute("""SELECT * FROM states WHERE name
-                    LIKE BINARY 'N%' ORDER BY states.id""")
-        
-        # Fetch and print results
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-        
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+        cursor = db.cursor()
+        cursor.execute("""SELECT * FROM states WHERE name
+                       LIKE BINARY 'N%' ORDER BY states.id""")
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+        cursor.close()
+        db.close()
     except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-    
-    finally:
-        # Close cursor and database connection
-        if 'cur' in locals() and cur is not None:
-            cur.close()
-        if 'db' in locals() and db is not None:
-            db.close()
+        print("MySQL Error:", e)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        filter_states(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        print("Usage: ./1-filter_states.py <username> <password> <database>")
